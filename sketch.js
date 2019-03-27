89aa3556b0269d66a77c68150127dc02720d6e6e
LlamaTemplar <cole.pergerson@gmail.com>
2019-03-27 15:55:02 -0700

    added p5

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+	// make canvas full screen
+	createCanvas(windowWidth, windowHeight);
+
+}
+
+
+function draw() {
+	// adding clear() to the draw loop will clear each frame, erasing object trails
+	// clear();
+
+	fill(210, 180, 140)
+	ellipse(windowWidth/2, windowHeight/2, 100);
+}
